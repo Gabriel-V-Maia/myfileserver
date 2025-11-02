c7c4f860ca5216fd27dee1e21b5df074a9402175
lordduck <gabrielvinicius.damaia@gmail.com>
2025-11-02 13:31:26 -0300

    refactor: uso do .env para guardar ip do server

--- client/push.py
+++ client/push.py
@@ -1,10 +1,20 @@
 import sys
 from send import FileClient
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+server_ip = os.getenv("server_ip")
+
+if not server_ip:
+    print("server_ip não configurado, abortando")
+    sys.exit(1)
 
 if len(sys.argv) < 2:
     print("uso: push <arquivo1> <arquivo2> ...")
     sys.exit(1)
 
 files = sys.argv[1:]
-client = FileClient(host="127.0.0.1", port=6000)
+client = FileClient(host=server_ip, port=6000)
 client.push(*files)
