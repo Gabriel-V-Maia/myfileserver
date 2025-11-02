f04031f6aea07e40666153064cb0625444f9a213
lordduck <gabrielvinicius.damaia@gmail.com>
2025-11-02 12:10:21 -0300

    feat: verificação para permissões root/admin

--- server/server.py
+++ server/server.py
@@ -1,6 +1,28 @@
 import socket
 import os
 from pathlib import Path
+import os
+import sys
+import ctypes
+import subprocess
+
+def ensure_admin():
+    if os.name == "nt":  # 
+        try:
+            is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            is_admin = False
+        if not is_admin:
+            params = " ".join([f'"{arg}"' for arg in sys.argv])
+            ctypes.windll.shell32.ShellExecuteW(
+                None, "runas", sys.executable, params, None, 1
+            )
+            sys.exit()
+    else:  
+        if os.geteuid() != 0:
+            print("[!] este script precisa ser rodado como root")
+            os.execvp("sudo", ["sudo", sys.executable] + sys.argv)
+
 
 class fileServer:
     def __init__(self, HOST="0.0.0.0", PORT=6000, STORAGEDIR="/fileserver/"):
