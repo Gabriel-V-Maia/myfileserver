40d12a64bc5672c1b2cc48db1a716aa621c4d8ee
Gabriel-V-Maia <gabrielvinicius.damaia@gmail.com>
2025-11-15 14:54:55 -0300

    refactor: não tente executar o script como root/admon

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 import ctypes
 
+import os
 from utils import Logger
-from server import FileServer
+from inputs import InputManagement
 
 def ensure_admin():
     if os.name == "nt":   
@@ -10,18 +11,14 @@ def ensure_admin():
         except:
             is_admin = False
         if not is_admin:
-            params = " ".join([f'"{arg}"' for arg in sys.argv])
-            ctypes.windll.shell32.ShellExecuteW(
-                None, "runas", sys.executable, params, None, 1
-            )
+            print("[!] Rode esse script como administrador")
             sys.exit()
     else:  
         if os.geteuid() != 0:
             print("[!] este script precisa ser rodado como root")
-            os.execvp("sudo", ["sudo", sys.executable] + sys.argv)
-
+            break
        
 if __name__ == "__main__":
     ensure_admin()  
-    fileserver = fileServer()
-    fileserver.start()
+    inputss = InputManagement("Input Manager")
+    inputss.start()
